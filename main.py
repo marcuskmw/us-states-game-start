fa9b9f6614dcf82b4efaef72d488f6cae9574db3
Marcus Wong <marcuswongkaiman@gmail.com>
2021-11-10 13:42:47 +0800

    save the not guested state into learn.csv

--- main.py
+++ main.py
@@ -7,16 +7,16 @@ image = "blank_states_img.gif"
 screen.addshape(image)
 turtle.shape(image)
 df = pandas.read_csv("50_states.csv")
+# df.set_index("state")
 key_pool = df.state.to_list()
 t = turtle.Turtle()
 t.hideturtle()
 t.penup()
 
-
-def write_record_to_screen(data_record, name_of_state):
-    t.goto(int(data_record.x), int(data_record.y))
-    t.write(name_of_state)
-
+# def write_record_to_screen(data_record, name_of_state):
+#     t.goto(int(data_record.x), int(data_record.y))
+#     t.write(name_of_state)
+#
 
 finish_game = False
 no_of_round = 0
@@ -29,9 +29,18 @@ while not finish_game:
         if answer_state in key_pool:
             record_df = df[df.state == answer_state]
             key_pool.remove(answer_state)
+
+            # df.drop(answer_state, axis=0)
             no_of_round += 1
-            write_record_to_screen(record_df, answer_state)
+            t.goto(int(record_df.x), int(record_df.y))
+            t.write(record_df.state.item())
+            # t.write(answer_state)
+            # write_record_to_screen(record_df, answer_state)
     else:
+        # end the game if user click cancel
         finish_game = True
 
-screen.exitonclick()
+
+# generate learn.csv
+learn_data_frame = pandas.DataFrame(key_pool, columns =['State'])
+learn_data_frame.to_csv("learn.csv")
